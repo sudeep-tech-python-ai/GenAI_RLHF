c63592a8810de124051c15a1276869270ff7f5d9
shindesudeep <206251900+sudeep-tech-python-ai@users.noreply.github.com>
2025-05-22 19:05:23 +0530

    langchain projects

---
+++ Langchain/Langchain_Projects/NVIDIANIM/app.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = os.getenv("NVIDIA_API_KEY")
+)
+
+completion = client.chat.completions.create(
+  model="meta/llama-3.3-70b-instruct",
+  messages=[{"role":"user","content":"provide me an article on machine learning"}],
+  temperature=0.2,
+  top_p=0.7,
+  max_tokens=1024,
+  stream=True
+)
+
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
+
